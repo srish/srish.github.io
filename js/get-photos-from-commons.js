26dea022a395e6dc05c707f8bcbe5885811f44ee
srish <srishakatux@gmail.com>
2018-12-27 23:33:41 -0800

    Fetch photos from Wikimedia Commons

---
+++ js/get-photos-from-commons.js
@@ -0,0 +1,22 @@
+var api_endpoint = 'https://commons.wikimedia.org/w/api.php?action=query&list=allimages&aiuser=Srishti%20Sethi&aisort=timestamp&ailimit=100&format=json';
+
+$.ajax( {
+	url: api_endpoint, 
+	dataType: 'jsonp',
+	success: function( data ) {
+		var results = data && data.query && data.query.allimages,
+			response = '',
+			name;
+
+		if ( results ) { 
+			for( var i = 0; i< results.length; i++ ) {
+				name = results[i].name.split( '.' )[0].replace( /_/g, ' ' );
+				response += '<div class="item"><img src="' + results[i].url + '"><div class="photos-carousel carousel-caption"><h5>' + name + '</h5><span><a href="'+ results[i].descriptionurl + '" target="_blank">View on Wikimedia Commons</a></span></div></div>';
+			}
+
+			$( '.carousel-inner' ).append( response );
+			$( '.item' ).first().addClass( 'active' );
+			$( '#photoCarousel' ).carousel();
+		}
+	}
+} );
