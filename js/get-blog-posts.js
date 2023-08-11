d82202143ea5698da50456ef487aee0e9586ec53
srish <srishakatux@gmail.com>
2023-08-11 01:54:49 -0700

    Change /writing to /articles; add/remove new articles

--- js/get-blog-posts.js
+++ js/get-blog-posts.js
@@ -1,13 +1,28 @@
 var posts = [
 	{
-		"title": "Reflecting on 2021 and preparing for better dating experiences in 2022",
-		"pubdate": "31 Dec 2021",
-		"link": "https://medium.com/@Srish_Aka_Tux/reflecting-on-2021-and-preparing-for-better-dating-experiences-in-2022-115cb711050e",
+		"title": "10 ways Wikimedia does developer advocacy (Part 2)",
+		"pubdate": "7 Mar 2023",
+		"link": "https://opensource.com/article/23/3/developer-advocacy-wikimedia",
 	},
 	{
-		"title": "Unstructured Studio 2021 ➔ Year in Review",
-		"pubdate": "30 Dec 2021",
-		"link": "https://unstructured.studio/blog/unstructured-studio-2021-year-in-review/",
+		"title": "How Wikipedia helps keep the internet open (Part 1)",
+		"pubdate": "6 Mar 2023",
+		"link": "https://opensource.com/article/23/3/how-wikipedia-helps-keep-internet-open",
+	},
+	{
+		"title": "Year-end recess in the Alaskan wilderness",
+		"pubdate": "29 Dec 2022",
+		"link": "https://medium.com/@Srish_Aka_Tux/year-end-recess-in-the-alaskan-wilderness-e2b07a20fcc9",
+	},
+	{
+		"title": "Unstructured Studio 2022 ➔ Year in Review",
+		"pubdate": "28 Dec 2022",
+		"link": "https://unstructured.studio/blog/unstructured-studio-2022-year-in-review/",
+	},
+	{
+		"title": "Reflecting on 2021 and preparing for better dating experiences in 2022",
+		"pubdate": "31 Dec 2021",
+		"link": "https://medium.com/@Srish_Aka_Tux/reflecting-on-2021-and-preparing-for-better-dating-experiences-in-2022-115cb711050e"
 	},
 	{
 		"title": "Creative learning residential camp in nature's lap, Umeta village (Gujarat)",
@@ -15,39 +30,9 @@ var posts = [
 		"link": "https://unstructured.studio/blog/creative-learning-in-natures-lap/",
 	},
 	{
-		"title": "Roundup: Google Summer of Code 2021 and Outreachy Round 22",
-		"pubdate": "8 Oct 2021",
-		"link": "https://techblog.wikimedia.org/2021/10/08/roundup-google-summer-of-code-2021-and-outreachy-round-22/",
-	},
-	{
-		"title": "Call for smaller language wikis to participate in the technical capacity-building program!",
-		"pubdate": "24 Sep 2021",
-		"link": "https://diff.wikimedia.org/2021/09/24/call-for-smaller-language-wikis-to-participate-in-the-technical-capacity-building-program/",
-	},
-	{
-		"title": "Our Tinkering Fellows share their experiences",
-		"pubdate": "30 Aug 2021",
-		"link": "https://unstructured.studio/blog/our-tinkering-fellows-share-their-experiences/",
-	},
-	{
-		"title": "Outreachy round 21: experiences and outcomes",
-		"pubdate": "2 Jun 2021",
-		"link": "https://techblog.wikimedia.org/2021/06/02/outreachy-round-21-experiences-and-outcomes/",
-	},
-	{
-		"title": "Our COVID-19 Support Programs",
-		"pubdate": "10 May 2021",
-		"link": "https://unstructured.studio/blog/covid-19-support-programs/",
-	},
-	{
-		"title": "Tinkering and Facilitation Capacity Building Workshop Series, Ahmedabad",
-		"pubdate": "17 Mar 2021",
-		"link": "https://unstructured.studio/blog/tinkering-and-facilitation-workshops-ahmedabad/",
-	},
-	{
-		"title": "Looking Back At 2020, Looking Ahead To 2021",
-		"pubdate": "3 Jan 2021",
-		"link": "https://medium.com/@Srish_Aka_Tux/looking-back-at-2020-looking-ahead-to-2021-77c0136a571c",
+		"title": "This Toronto-based non-profit provides creative education to over 2000 children in rural India (interview)",
+		"pubdate": "1 Sep 2021",
+		"link": "https://yourstory.com/socialstory/2021/08/toronto-non-profit-unstructured-studio-rural-india-creativity-2000-children"
 	},
 	{
 		"title": "One year of Small Wiki Toolkits initiative–and more to come!",
@@ -55,29 +40,14 @@ var posts = [
 		"link": "https://diff.wikimedia.org/2020/09/30/one-year-of-small-wiki-toolkits-initiative-and-more-to-come/",
 	},
 	{
-		"title": "Kriti Program (pilot report)",
+		"title": "Kriti Program: learning remotely via low-cost making and tinkering! (pilot report)",
 		"pubdate": "18 Sep 2020",
 		"link": "https://unstructured.studio/uploads/2020/09/kriti-program-report.pdf",
-	},
-	{
-		"title": "Kriti Program: learning remotely via low-cost making and tinkering!",
-		"pubdate": "18 Sep 2020",
-		"link": "https://unstructured.studio/blog/kriti-program-learning-remotely-via-low-cost-making-and-tinkering/",
 	},
 	{
 		"title": "Lalitha’s story: an Outreachy intern shares her experience",
 		"pubdate": "26 Aug 2020",
 		"link": "https://techblog.wikimedia.org/2020/08/26/lalithas-story-an-outreachy-intern-shares-her-experience/",
-	},
-	{
-		"title": "Designing technical workshops for the Indic community",
-		"pubdate": "11 Aug 2020",
-		"link": "https://diff.wikimedia.org/2020/08/11/designing-technical-workshops-for-the-indic-community/",
-	},
-	{
-		"title": "Creative learning during COVID-19",
-		"pubdate": "05 May 2020",
-		"link": "https://unstructured.studio/blog/creative-learning-during-covid-19/",
 	},
 	{
 		"title": "Using Zulip, an Open Source tool, for engaging participants in Wikimedia’s technical outreach programs",
@@ -101,13 +71,8 @@ var posts = [
 	},
 	{
 		"title": "Year in Review 2019",
-		"pubdate": "31 Dec 2019",
+		"pubdate": "Dec 30, 2019",
 		"link": "https://medium.com/@Srish_Aka_Tux/year-in-review-2019-7171b25053b6",
-	},
-	{
-		"title": "Round-up of Small Wiki Toolkits at Wikimania 2019",
-		"pubdate": "24 Sep 2019",
-		"link": "https://discuss-space.wmflabs.org/t/round-up-of-small-wiki-toolkits-at-wikimania-2019/1447",
 	},
 	{
 		"title": "Wikimania Stockholm 2019",
@@ -145,11 +110,6 @@ var posts = [
 		"link": "https://medium.com/@Srish_Aka_Tux/gerrit-newcomer-bot-one-small-step-to-welcoming-new-developers-and-tracking-their-patches-50cc6eebf34d",
 	},
 	{
-		"title": "See the completed Google Summer of Code and Outreachy projects from this year",
-		"pubdate": "27 Aug 2018",
-		"link": "https://wikimediafoundation.org/2018/08/27/google-summer-of-code-outreachy-round-16/",
-	},
-	{
 		"title": "IRCTC, the lifeline of train ticket booking looks dead from outside",
 		"pubdate": "13 Aug 2018",
 		"link": "https://medium.com/@Srish_Aka_Tux/irctc-the-lifeline-of-train-ticket-booking-looks-dead-from-outside-e5d836e8c22c",
@@ -165,21 +125,6 @@ var posts = [
 		"link": "https://medium.com/@Srish_Aka_Tux/volunteering-at-scripted-what-i-knew-taught-and-learned-b8174545b8d0",
 	},
 	{
-		"title": "Wikimedia Foundation funds six Outreachy interns for round 15",
-		"pubdate": "01 Dec 2017",
-		"link": "https://blog.wikimedia.org/2017/12/01/six-outreachy-interns-funded/",
-	},
-	{
-		"title": "What ten students made for Wikimedia while participating in Google Summer of Code and Outreachy round 14",
-		"pubdate": "06 Sep 2017",
-		"link": "https://blog.wikimedia.org/2017/09/06/gsoc-outreachy-wikimedia-2017/",
-	},
-	{
-		"title": "What new I learned in the Wikimedia movement at Wikimania’17",
-		"pubdate": "19 Aug 2017",
-		"link": "https://medium.com/@Srish_Aka_Tux/what-new-i-learned-in-the-wikimedia-movement-at-wikimania17-e90870e1a91a",
-	},
-	{
 		"title": "What we learned by making both newcomers and experienced participants feel connected and engaged at the Vienna Hackathon",
 		"pubdate": "31 May 2017",
 		"link": "https://blog.wikimedia.org/2017/05/31/vienna-hackathon-learnings/",
@@ -188,11 +133,6 @@ var posts = [
 		"title": "Tale of a random act of kindness from a biking trip in California",
 		"pubdate": "10 May 2017",
 		"link": "https://medium.com/@Srish_Aka_Tux/tale-of-a-random-act-of-kindness-from-a-biking-trip-in-california-6f31c905d323",
-	},
-	{
-		"title": "Looking back on the 2017 Wikimedia Developer Summit",
-		"pubdate": "24 Mar 2017",
-		"link": "https://blog.wikimedia.org/2017/03/24/wikimedia-developer-summit-2017/",
 	},
 	{
 		"title": "Some methods I learned from Aspiration Tech for running in-person participatory events",
@@ -223,11 +163,6 @@ var posts = [
 		"title": "Trip Report: Lifelong Learning @Design Innovation Workshop, Ahmedabad 2015",
 		"pubdate": "16 Feb 2015",
 		"link": "http://learn.media.mit.edu/reports/indiavisit/",
-	},
-	{
-		"title": "Second Virtual Visit to the Media Lab",
-		"pubdate": "30 Sep 2014",
-		"link": "https://medium.com/@Srish_Aka_Tux/second-virtual-visit-to-the-media-lab-606a28a1b9e9",
 	},
 	{
 		"title": "Learning Over Education: Connecting Indian undergraduates outside of university lectures",
